4268adb6241d37b6ae7aa92adca2bdddbc44b5ab
hiba2004-cell <hiba.nadiri04@gmail.com>
2025-10-09 16:05:14 +0100

    rashid

--- lib/auth/register/register.dart
+++ lib/auth/register/register.dart
@@ -10,34 +10,33 @@ import 'package:nurox_chat/components/text_form_builder.dart';
 import 'package:nurox_chat/utils/validation.dart';
 import 'package:nurox_chat/view_models/auth/register_view_model.dart';
 import 'package:nurox_chat/widgets/indicators.dart';
-
+// classe register 
 class Register extends StatefulWidget {
   @override
-  _RegisterState createState() => _RegisterState();
+  _RegisterState createState() => _RegisterState();  // Création de l'état associé
 }
 
 class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
-    RegisterViewModel viewModel = Provider.of<RegisterViewModel>(context);
+    RegisterViewModel viewModel = Provider.of<RegisterViewModel>(context);   // On récupère le ViewModel via Provider
     return LoadingOverlay(
       progressIndicator: circularProgress(context),
-      isLoading: viewModel.loading,
+      isLoading: viewModel.loading,  // Indicateur de chargement affiché lors du traitement d'inscription
       child: Scaffold(
         appBar: AppBar(
           automaticallyImplyLeading:
-              true, // enables the back arrow automatically
+              true, // affiche automatiquement la flèche de retour
           elevation: 0,
-          backgroundColor: Colors.transparent, // optional for a clean look
-          iconTheme: IconThemeData(color: Colors.black), // arrow color
-          
+          backgroundColor: Colors.transparent, 
+          iconTheme: IconThemeData(color: Colors.black), 
         ),
-        key: viewModel.scaffoldKey,
-        body: ListView(
+        key: viewModel.scaffoldKey,  // clé pour gérer les messages
+        body: ListView(  // Corps principal de la page
           padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
           children: [
             SizedBox(height: MediaQuery.of(context).size.height / 27),
-            Text(
+            Text(  // Message d'accueil principal
               'Welcome to Raja Chat\nCreate a new account and connect with friends',
               style: GoogleFonts.nunitoSans(
                 fontWeight: FontWeight.bold,
@@ -45,15 +44,15 @@ class _RegisterState extends State<Register> {
               ),
             ),
             SizedBox(height: 30.0),
-            buildForm(viewModel, context),
+            buildForm(viewModel, context),   // Appel du formulaire d'inscription
             SizedBox(height: 30.0),
-            Row(
+            Row(    // Lien vers la page de connexion
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
                   'Already have an account ? ',
                 ),
-                GestureDetector(
+                GestureDetector(   // Texte cliquable pour retourner à la page précédente (Login)
                   onTap: () => Navigator.pop(context),
                   child: Text(
                     'Login',
@@ -74,25 +73,25 @@ class _RegisterState extends State<Register> {
   buildForm(RegisterViewModel viewModel, BuildContext context) {
     return Form(
       key: viewModel.formKey,
-      autovalidateMode: AutovalidateMode.onUserInteraction,
+      autovalidateMode: AutovalidateMode.onUserInteraction,  // validation automatique
       child: Column(
         children: [
-          TextFormBuilder(
+          TextFormBuilder(  // // Champ : Nom d'utilisateur
             enabled: !viewModel.loading,
             prefix: Ionicons.person_outline,
             hintText: "Username",
             textInputAction: TextInputAction.next,
             validateFunction: Validations.validateName,
             onSaved: (String val) {
-              viewModel.setName(val);
+              viewModel.setName(val);  // sauvegarde du nom dans le ViewModel
             },
-            focusNode: viewModel.usernameFN,
-            nextFocusNode: viewModel.emailFN,
+            focusNode: viewModel.usernameFN,   // focus actuel
+            nextFocusNode: viewModel.emailFN, // focus suivant
           ),
           SizedBox(height: 20.0),
-          TextFormBuilder(
+          TextFormBuilder(     //Champ : Email
             enabled: !viewModel.loading,
-            prefix: Ionicons.mail_outline,
+            prefix: Ionicons.mail_outline,  
             hintText: "Email",
             textInputAction: TextInputAction.next,
             validateFunction: Validations.validateEmail,
@@ -103,9 +102,9 @@ class _RegisterState extends State<Register> {
             nextFocusNode: viewModel.countryFN,
           ),
           SizedBox(height: 20.0),
-          TextFormBuilder(
+          TextFormBuilder(  //  Champ : Pays
             enabled: !viewModel.loading,
-            prefix: Ionicons.pin_outline,
+            prefix: Ionicons.pin_outline,  //icône localisation
             hintText: "Country",
             textInputAction: TextInputAction.next,
             validateFunction: Validations.validateName,
@@ -116,14 +115,14 @@ class _RegisterState extends State<Register> {
             nextFocusNode: viewModel.passFN,
           ),
           SizedBox(height: 20.0),
-          PasswordFormBuilder(
+          PasswordFormBuilder(  //  Champ : Mot de passe
             enabled: !viewModel.loading,
-            prefix: Ionicons.lock_closed_outline,
-            suffix: Ionicons.eye_outline,
+            prefix: Ionicons.lock_closed_outline, //icône cadenas fermé
+            suffix: Ionicons.eye_outline,   //  icône œil pour afficher/masquer le mot de pass
             hintText: "Password",
             textInputAction: TextInputAction.next,
             validateFunction: Validations.validatePassword,
-            obscureText: true,
+            obscureText: true, // cache le texte
             onSaved: (String val) {
               viewModel.setPassword(val);
             },
@@ -131,13 +130,13 @@ class _RegisterState extends State<Register> {
             nextFocusNode: viewModel.cPassFN,
           ),
           SizedBox(height: 20.0),
-          PasswordFormBuilder(
+          PasswordFormBuilder(  // Champ : Confirmation du mot de passe
             enabled: !viewModel.loading,
             prefix: Ionicons.lock_open_outline,
             hintText: "Confirm Password",
             textInputAction: TextInputAction.done,
             validateFunction: Validations.validatePassword,
-            submitAction: () => viewModel.register(context),
+            submitAction: () => viewModel.register(context), // exécute la fonction d'inscription
             obscureText: true,
             onSaved: (String val) {
               viewModel.setConfirmPass(val);
@@ -145,7 +144,7 @@ class _RegisterState extends State<Register> {
             focusNode: viewModel.cPassFN,
           ),
           SizedBox(height: 25.0),
-          Container(
+          Container( // Bouton "Sign Up"
             height: 45.0,
             width: 180.0,
             child: ElevatedButton(
@@ -166,7 +165,7 @@ class _RegisterState extends State<Register> {
                   fontWeight: FontWeight.w600,
                 ),
               ),
-              onPressed: () => viewModel.register(context),
+              onPressed: () => viewModel.register(context),       // Action au clic : appel à la méthode register() du ViewModel
             ),
           ),
         ],
